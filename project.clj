70f42b64cc470e1d6f5b9ae67aee2774803409e7
zsau <zsau@fastmail.com>
2019-11-24 18:00:13 -0800

    alphabetize

--- project.clj
+++ project.clj
@@ -7,13 +7,13 @@
 	:aot [feedmail]
 	:main feedmail
 	:dependencies [
+		[ch.qos.logback/logback-classic "1.2.3"]
+		[clj-http "3.9.1"]
+		[clojure.java-time "0.3.2"]
+		[com.taoensso/timbre "4.10.0"]
+		[enlive "1.1.6"]
 		[org.clojure/clojure "1.9.0"]
 		[org.clojure/core.memoize "0.7.1"]
 		[org.clojure/tools.cli "0.4.1"]
-		[clojure.java-time "0.3.2"]
-		[clj-http "3.9.1"]
-		[enlive "1.1.6"]
-		[com.taoensso/timbre "4.10.0"]
 		[zsau/feedparser-clj "0.6.2"]
-		[ch.qos.logback/logback-classic "1.2.3"]
 		[zsau/mail "0.1.0"]])
