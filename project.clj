863abcc201b4e54dcfd1900a429df6222019a4e9
zsau <zsau@fastmail.com>
2020-07-22 13:50:28 -0700

    update deps

--- project.clj
+++ project.clj
@@ -15,5 +15,5 @@
 		[org.clojure/clojure "1.10.1"]
 		[org.clojure/core.memoize "1.0.236"]
 		[org.clojure/tools.cli "1.0.194"]
-		[zsau/rome-clj "1.0.0"]
+		[zsau/rome-clj "1.0.1"]
 		[zsau/mail "0.1.1"]])
